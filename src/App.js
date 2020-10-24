695bc48b37ff614b4911d740f3619fea2e781866
D <69029204+d1anas0@users.noreply.github.com>
2020-10-24 18:37:05 +1100

    start Journal page (#1)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-
+import Journal from './Journal';
 
 function App() {
   return (
     <div className="App">
-        <Button variant="primary">Primary</Button>{' '}
+        <Journal/>
     </div>
   );
 }
